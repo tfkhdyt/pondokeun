b64aa99f6c37a28816df98068485d2c603fe5938
tfkhdyt <tfkhdyt@proton.me>
2023-01-21 10:38:30 +0700

    feat(routes): add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '@picocss/pico';
+</script>
+
+<main class="container">
+	<slot />
+</main>
+
+<style>
+	:root {
+		--font-size: 0.98rem;
+	}
+
+	main {
+		padding: 2rem 0;
+	}
+</style>
