741cb03d16cab203af36b2d7820fe6866255c831
tfkhdyt <tfkhdyt@proton.me>
2023-01-21 19:03:07 +0700

    feat(routes): add slug page

---
+++ src/routes/[slug]/+page.server.ts
@@ -0,0 +1,21 @@
+import { db } from '$lib/database/prisma';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ params }) => {
+	const { slug } = params;
+
+	const link = await db.link.findFirst({
+		where: {
+			slug
+		}
+	});
+
+	if (!link) {
+    return {
+      message: 'Page not found'
+    };
+	}
+
+  throw redirect(301, link.link);
+}) satisfies PageServerLoad;
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+</script>
+
+<main>
+	{#if data.message}
+		<h1>404</h1>
+		<h2>{data.message}</h2>
+  {:else}
+    <h2>Redirecting</h2>
+	{/if}
+</main>
