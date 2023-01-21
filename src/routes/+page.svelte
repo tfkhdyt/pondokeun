f9561b2c9a077b7c7a1e4c5201aaa253db25d196
tfkhdyt <tfkhdyt@proton.me>
2023-01-21 10:38:56 +0700

    feat(routes): add main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	export let form: ActionData;
+
+	$: console.log(form);
+</script>
+
+<svelte:head>
+	<title>Pondokeun</title>
+</svelte:head>
+
+<h1>Pondokeun</h1>
+
+<article>
+	<form method="POST" use:enhance>
+		<label for="link"
+			>Input your link
+			{#if form?.missing}
+				<b>Link is required</b>
+			{/if}
+		</label>
+		<input type="url" id="link" name="link" placeholder="https://..." />
+
+		<button>Submit</button>
+	</form>
+</article>
+
+<style>
+	article {
+		max-width: 768px;
+		margin: 0 auto;
+		--primary: #3949ab;
+		--primary-hover: #323e84;
+	}
+
+  label {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  label > b {
+    color: red;
+  }
+</style>
