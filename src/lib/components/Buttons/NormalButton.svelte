fc9a1f6d85b03b34993faa534aecbf21eb644b99
tfkhdyt <tfkhdyt@proton.me>
2023-01-22 09:41:17 +0700

    fix(normal button): set customClass default to empty string

--- src/lib/components/Buttons/NormalButton.svelte
+++ src/lib/components/Buttons/NormalButton.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  export let customClass: string;
+  export let customClass = '';
 </script>
 
 <button
-	class="inline-block rounded py-3 text-sm font-medium text-white transition duration-500 ease-in-out focus:outline-none focus:ring w-2/6 active:ring-0 {customClass}"
+	class="inline-block rounded py-3 text-sm font-medium transition duration-500 ease-in-out focus:outline-none focus:ring w-2/6 active:ring-0 {customClass}"
 >
   <slot />
 </button>
