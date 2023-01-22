734a2c527f7771961d47dde5fbf48be9e21f6931
tfkhdyt <tfkhdyt@proton.me>
2023-01-22 10:01:15 +0700

    fix(card): set w-[32rem] to md breakpoint

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,5 +1,5 @@
 <div
-	class="rounded-2xl bg-gradient-to-br from-yellow-400 to-red-600 p-1 shadow-xl w-[32rem] dark:shadow-gray-700/50 dark:from-yellow-600 dark:to-red-800"
+	class="rounded-2xl bg-gradient-to-br from-yellow-400 to-red-600 p-1 shadow-xl dark:shadow-gray-700/50 dark:from-yellow-600 dark:to-red-800 m-4 md:w-[32rem]"
 >
 	<div class="block rounded-xl bg-white dark:bg-gray-800 p-6 sm:p-8"><slot /></div>
 </div>
