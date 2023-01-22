178d47149057d82398aadb65aee047d8e98aef2b
tfkhdyt <tfkhdyt@proton.me>
2023-01-22 10:01:34 +0700

    feat(input field): add customClass prop

--- src/lib/components/InputField.svelte
+++ src/lib/components/InputField.svelte
@@ -2,6 +2,7 @@
   export let type = 'text';
   export let name: string;
   export let placeholder: string;
+  export let customClass = '';
 </script>
 
 <input
@@ -9,5 +10,5 @@
   {name}
   {placeholder}	
   required
-	class="p-4 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm focus:border-yellow-600 focus:ring-yellow-600 w-4/6 dark:text-gray-300 transition duration-500 ease-in-out"
+	class="p-4 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm focus:border-yellow-600 focus:ring-yellow-600 dark:text-gray-300 transition duration-500 ease-in-out {customClass}"
 />
