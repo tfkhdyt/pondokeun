26974dfbfe7ab6784eb4984710970bb3b54af811
tfkhdyt <tfkhdyt@proton.me>
2023-01-23 07:30:38 +0700

    fix(normal button): remove w-2/6 class

--- src/lib/components/Buttons/NormalButton.svelte
+++ src/lib/components/Buttons/NormalButton.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <button
-	class="inline-block rounded py-3 text-sm font-medium transition duration-500 ease-in-out focus:outline-none focus:ring w-2/6 active:ring-0 {customClass}"
+	class="inline-block rounded py-3 text-sm font-medium transition duration-500 ease-in-out focus:outline-none focus:ring active:ring-0 {customClass}"
 >
   <slot />
 </button>
