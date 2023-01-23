89d53136c4951fedf7575f7c760edd50fd50be31
tfkhdyt <tfkhdyt@proton.me>
2023-01-23 07:30:52 +0700

    feat(input field): add pattern prop

--- src/lib/components/InputField.svelte
+++ src/lib/components/InputField.svelte
@@ -3,12 +3,14 @@
   export let name: string;
   export let placeholder: string;
   export let customClass = '';
+  export let pattern: string | undefined = undefined;
 </script>
 
 <input
   {type}
   {name}
   {placeholder}	
+  {pattern}
   required
 	class="p-4 w-full rounded-md border-gray-200 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white sm:text-sm focus:border-yellow-600 focus:ring-yellow-600 dark:text-gray-300 transition duration-500 ease-in-out {customClass}"
 />
