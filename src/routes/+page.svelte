b439df17558cbc98fc63a502d7d06919743f2756
tfkhdyt <tfkhdyt@proton.me>
2023-01-23 16:17:41 +0700

    refactor(root page): add MyLinks components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
 	import Header1 from '$lib/components/Headers/Header1.svelte';
 	import Hr from '$lib/components/Hr.svelte';
 	import InputField from '$lib/components/InputField.svelte';
+	import MyLinks from '$lib/components/MyLinks.svelte';
 	import Profile from '$lib/components/Profile.svelte';
 	import Tooltip from '$lib/components/Tooltip.svelte';
 
@@ -142,87 +143,7 @@
 			{/if}
 		</Card>
 		{#if session}
-			<Card customClass="lg:col-span-2">
-				<Header1>My Links</Header1>
-				<div>
-					<div class="overflow-x-auto">
-						<table class="min-w-full divide-y-2 divide-gray-200 text-sm dark:divide-gray-700">
-							<thead>
-								<tr>
-									<th
-										class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-300"
-									>
-										Slug
-									</th>
-									<th
-										class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-300"
-									>
-										Original url
-									</th>
-									<th
-										class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-300"
-									>
-										Created date
-									</th>
-									<th
-										class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-300"
-									>
-										Updated date
-									</th>
-									<th
-										class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-300"
-									>
-										Actions
-									</th>
-								</tr>
-							</thead>
-
-							<tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-								{#each data.links as link (link.id)}
-									<tr>
-										<td
-											class="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-gray-300"
-										>
-											<a
-												href={`${appUrl}/${link.slug}`}
-												class="underline"
-												target="_blank"
-												rel="noreferrer"
-											>
-												/{link.slug}
-											</a>
-										</td>
-										<td class="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-											{link.link}
-										</td>
-										<td class="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-											{new Date(link.created_at).toLocaleString('id-ID')}
-										</td>
-										<td class="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-											{new Date(link.updated_at).toLocaleString('id-ID')}
-										</td>
-										<td class="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-											<Tooltip label="Copied">
-												<NormalButton
-													customClass="bg-blue-500 hover:bg-blue-600 text-white px-2"
-													onClick={() => navigator.clipboard.writeText(`${appUrl}/${link.slug}`)}
-													>Copy</NormalButton
-												>
-											</Tooltip>
-											<NormalButton customClass="bg-yellow-500 hover:bg-yellow-600 text-white px-2"
-												>Edit</NormalButton
-											>
-											<NormalButton customClass="bg-pink-500 hover:bg-pink-600 text-white px-2"
-												>Delete</NormalButton
-											>
-										</td>
-									</tr>
-								{/each}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</Card>
+      <MyLinks links={data.links} />		
 		{/if}
 	</div>
 </main>
