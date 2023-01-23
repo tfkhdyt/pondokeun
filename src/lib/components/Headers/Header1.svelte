aa94537834d1bc0465cdbb124ecb50f6faf25beb
tfkhdyt <tfkhdyt@proton.me>
2023-01-23 13:10:12 +0700

    feat(header1): add leading-normal class

--- src/lib/components/Headers/Header1.svelte
+++ src/lib/components/Headers/Header1.svelte
@@ -1,5 +1,5 @@
 <h1
-	class="text-4xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-br from-yellow-400 to-red-600 mb-8 dark:from-yellow-600 dark:to-red-800"
+	class="text-4xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-br from-yellow-400 to-red-600 mb-8 dark:from-yellow-600 dark:to-red-800 leading-normal"
 >
   <slot />
 </h1>
