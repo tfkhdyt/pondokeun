bcbebed0b8a1afc189aedd200b090cc4ecd510fc
tfkhdyt <tfkhdyt@proton.me>
2023-01-23 08:54:31 +0700

    feat(hooks.server): add adapter for prisma

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,16 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
 import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import { db } from '$lib/database/prisma';
 
 export const handle = SvelteKitAuth({
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
+	adapter: PrismaAdapter(db),
+	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+	session: {
+		generateSessionToken: () => {
+			return crypto.randomUUID();
+		}
+	}
 });
