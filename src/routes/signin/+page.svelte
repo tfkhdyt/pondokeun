27c1da2e43c17ac0242635e530e023d78e30192d
tfkhdyt <tfkhdyt@proton.me>
2023-03-23 21:39:03 +0700

    feat(routes): add signin page

---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Card, Button } from 'flowbite-svelte';
+</script>
+
+<Card class="mx-auto">
+	<h3 class="p-0 mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign In</h3>
+	<form class="flex flex-col space-y-3">
+		<Button class="w-full" color="dark">Sign in with GitHub</Button>
+		<Button class="w-full" color="light">Sign in with Google</Button>
+	</form>
+</Card>
