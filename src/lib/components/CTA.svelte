b6bdcbecf1b2f3ee869093b2d958fe229586de67
tfkhdyt <tfkhdyt@proton.me>
2023-03-23 18:01:54 +0700

    refactor(cta): change buttons to flowbite-svelte components

--- src/lib/components/CTA.svelte
+++ src/lib/components/CTA.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { Button } from 'flowbite-svelte';
+</script>
+
 <div class="mb-4 space-y-6 text-center md:mb-8">
 	<h1 class="text-4xl font-bold">
 		Free and Open Source <span class="text-[#1a56db]">Link Shortener</span>
@@ -5,19 +9,10 @@
 	<h2 class="text-xl font-medium">
 		Link shortening made easy, secure, and free with the transparency of open source.
 	</h2>
-	<div class="flex justify-center pt-1 md:pt-2">
-		<a
-			href="/signin"
-			type="button"
-			class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-white bg-blue-700 rounded-lg md:py-3 md:text-base dark:bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-			>Get Started for Free</a
-		>
-		<a
-			href="https://github.com/tfkhdyt/pondokeun"
-			target="_blank"
-			type="button"
-			class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-300 md:py-3 md:text-base dark:text-white dark:bg-gray-800 dark:border-gray-600 hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 focus:outline-none dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-			>Self Host from Source</a
+	<div class="flex justify-center pt-1 space-x-2 md:pt-2">
+		<Button size="md" href="/signin" pill>Get Started for Free</Button>
+		<Button size="md" href="https://github.com/tfkhdyt/pondokeun" target="_blank" pill color="light"
+			>Self Host from Source</Button
 		>
 	</div>
 </div>
