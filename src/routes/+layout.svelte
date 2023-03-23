bc1ac309bfa4afb34c3269eacd7072343ca94a76
tfkhdyt <tfkhdyt@proton.me>
2023-03-23 21:39:56 +0700

    feat(root-layout): add autoAnimate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import '../app.css';
 	import 'flowbite/dist/flowbite.css';
+	import autoAnimate from '@formkit/auto-animate';
 
 	import Header from '$lib/components/Header.svelte';
 </script>
 
 <Header />
-<main class="container p-6 mx-auto md:p-8 md:max-w-screen-md">
+<main class="container p-6 mx-auto md:p-8 md:max-w-screen-md" use:autoAnimate>
 	<slot />
 </main>
