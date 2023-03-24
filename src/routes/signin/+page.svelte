24255d1677fb0059fa05d719bc02e7d5a070505a
tfkhdyt <tfkhdyt@proton.me>
2023-03-24 10:18:28 +0700

    feat(signin): add page title and make github login button works

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import { Card, Button } from 'flowbite-svelte';
+	import { signIn } from '@auth/sveltekit/client';
 </script>
+
+<svelte:head>
+	<title>Sign In | Pondokeun</title>
+</svelte:head>
 
 <Card class="mx-auto">
 	<h3 class="p-0 mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign In</h3>
 	<form class="flex flex-col space-y-3">
-		<Button class="w-full" color="dark">Sign in with GitHub</Button>
+		<Button class="w-full" color="dark" on:click={() => signIn('github')}
+			>Sign in with GitHub</Button
+		>
 		<Button class="w-full" color="light">Sign in with Google</Button>
 	</form>
 </Card>
