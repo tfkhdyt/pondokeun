4d77edf7810f70589e54f3646a6e18a664e1eb9b
tfkhdyt <tfkhdyt@proton.me>
2023-03-24 10:18:05 +0700

    feat(signin): redirect to home page if already signed in

---
+++ src/routes/signin/+page.server.ts
@@ -0,0 +1,9 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const session = await locals.getSession();
+	if (session?.user) {
+		throw redirect(301, '/');
+	}
+};
