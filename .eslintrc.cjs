e8631d46491781462ae399e2c82b7db1e3938821
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 19:26:33 +0700

    chore(eslint): setup unused-imports plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint', 'simple-import-sort'],
+	plugins: ['svelte3', '@typescript-eslint', 'simple-import-sort', 'unused-imports'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
@@ -20,5 +20,11 @@ module.exports = {
 	rules: {
 		'simple-import-sort/imports': 'error',
 		'simple-import-sort/exports': 'error',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+		],
 	},
 };
