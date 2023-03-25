c588900c82a24871a77ca46daa2278cb2889c8b6
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 19:28:08 +0700

    fix(server-hooks): add workaround for auth providers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,13 @@
+import type { Provider } from '@auth/core/providers';
 import GitHub from '@auth/core/providers/github';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import type { Handle } from '@sveltejs/kit';
 
 import { db } from '$db/prisma';
 import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
 
 export const handle = SvelteKitAuth({
 	adapter: PrismaAdapter(db),
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
-	// session: {
-	// 	generateSessionToken: () => {
-	// 		return crypto.randomUUID();
-	// 	}
-	// }
-});
+	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })] as Provider[],
+}) satisfies Handle;
