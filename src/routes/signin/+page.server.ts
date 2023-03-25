1178d59cdcb9c27c08decf8f33cb9667dbaccca6
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 08:16:04 +0700

    perf(signin-server): get session from parent instead of locals

--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,8 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	const session = await locals.getSession();
+export const load: PageServerLoad = async ({ parent }) => {
+	const { session } = await parent();
+
 	if (session?.user) {
 		throw redirect(301, '/');
 	}
