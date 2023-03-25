6230632f45507cae6f4f7948ecce2a566733cb5d
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 13:55:11 +0700

    fix(link-entity): change url validation message to 'Invalid URL'

--- src/entities/link.entity.ts
+++ src/entities/link.entity.ts
@@ -3,6 +3,6 @@ import { z } from 'zod';
 export const linkSchema = z.object({
 	link: z
 		.string({ required_error: 'Link is required!', invalid_type_error: 'Invalid type' })
-		.url({ message: 'Invalid url' }),
+		.url({ message: 'Invalid URL' }),
 	customName: z.string().optional()
 });
