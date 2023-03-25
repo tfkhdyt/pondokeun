b4c1d05db922ed367d05862c72fffa62dc828c65
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 13:24:24 +0700

    feat(header): add sign out icon next to sign out button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -39,7 +39,23 @@
 							<span class="block text-sm font-light truncate">{$page.data.session.user?.email}</span
 							>
 						</DropdownHeader>
-						<DropdownItem on:click={() => signOut()}>Sign out</DropdownItem>
+						<DropdownItem on:click={() => signOut()} class="flex">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="mr-1 w-5 h-5"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+								/>
+							</svg>
+							Sign out</DropdownItem
+						>
 					</Dropdown>
 				{/if}
 			</div>
