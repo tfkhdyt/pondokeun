dbbc43e4b309946e0a4fd1e73c0bad72ce48392b
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 13:27:26 +0700

    build(deps): install flowbite-svelte-blocks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,8 @@
 module.exports = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte-blocks/**/*.{html,js,svelte,ts}'
 	],
 	theme: {
 		extend: {}
