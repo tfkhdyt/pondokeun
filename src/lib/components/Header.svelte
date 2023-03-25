a81088c055e1361348e1d87f9ddbaec2caa112f9
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 13:56:06 +0700

    fix(header): change sign in icon

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -54,7 +54,7 @@
 									d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
 								/>
 							</svg>
-							Sign out</DropdownItem
+							Sign Out</DropdownItem
 						>
 					</Dropdown>
 				{/if}
@@ -67,12 +67,12 @@
 					viewBox="0 0 24 24"
 					stroke-width="1.5"
 					stroke="currentColor"
-					class="mr-2 w-5 h-5"
+					class="mr-1 w-5 h-5"
 				>
 					<path
 						stroke-linecap="round"
 						stroke-linejoin="round"
-						d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+						d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
 					/>
 				</svg>
 				Sign In</Button
