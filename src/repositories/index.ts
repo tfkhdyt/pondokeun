703529c2121108572e3fbcdca97fc96fe0dcd48a
tfkhdyt <tfkhdyt@proton.me>
2023-03-25 10:10:27 +0700

    refactor(link-repo): remove db from LinkRepository interface

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,6 +1,5 @@
-import type { PrismaClient, Link, Prisma } from '@prisma/client';
+import type { Link, Prisma } from '@prisma/client';
 
 export interface LinkRepository {
-	db: PrismaClient;
 	createLink(payload: Prisma.LinkCreateInput): Promise<[Link, Error | null]>;
 }
--- src/repositories/link/postgres.ts
+++ src/repositories/link/postgres.ts
@@ -2,11 +2,7 @@ import type { Link, Prisma, PrismaClient } from '@prisma/client';
 import type { LinkRepository } from '..';
 
 export default class LinkRepositoryPostgres implements LinkRepository {
-	db: PrismaClient;
-
-	constructor(db: PrismaClient) {
-		this.db = db;
-	}
+	constructor(private readonly db: PrismaClient) {}
 
 	async createLink(payload: Prisma.LinkCreateInput): Promise<[Link, Error | null]> {
 		const addedLink = await this.db.link.create({
