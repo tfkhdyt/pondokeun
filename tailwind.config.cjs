e7be3ad0c5e026613f3fc6d7d69811f17d310c03
tfkhdyt <tfkhdyt@proton.me>
2023-03-26 11:36:20 +0700

    chore(tailwind): add line clamp plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,6 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require('flowbite/plugin')],
+	plugins: [require('flowbite/plugin'), require('@tailwindcss/line-clamp')],
 	darkMode: 'class',
 };
