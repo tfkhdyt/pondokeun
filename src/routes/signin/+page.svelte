ec9e3b0f168b0e8fb4b9803eaa917b3a46f8c94a
tfkhdyt <tfkhdyt@proton.me>
2023-03-26 13:37:05 +0700

    refactor(signin): create component for sign in buttons

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import { signIn } from '@auth/sveltekit/client';
 	import { Card } from 'flowbite-svelte';
+
+	import FacebookSignInButton from '$lib/components/SignInButtons/FacebookSignInButton.svelte';
+	import GitHubSignInButton from '$lib/components/SignInButtons/GitHubSignInButton.svelte';
+	import GoogleSignInButton from '$lib/components/SignInButtons/GoogleSignInButton.svelte';
 </script>
 
 <svelte:head>
@@ -10,88 +13,10 @@
 <Card class="mx-auto">
 	<h3 class="p-0 mb-4 text-xl font-bold text-gray-900 dark:text-white">Sign In</h3>
 	<form class="flex flex-col space-y-3">
-		<ul class="my-4 space-y-3">
-			<li>
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<div
-					on:click={() => signIn('github')}
-					class="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg cursor-pointer dark:text-white dark:bg-gray-600 hover:bg-gray-100 hover:shadow group dark:hover:bg-gray-500">
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5"
-						><title>GitHub</title><path
-							d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
-					<span class="flex-1 ml-3 whitespace-nowrap">Sign in with GitHub</span>
-				</div>
-			</li>
-			<li>
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<div
-					on:click={() => signIn('google')}
-					class="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg cursor-pointer dark:text-white dark:bg-gray-600 hover:bg-gray-100 hover:shadow group dark:hover:bg-gray-500">
-					<svg
-						width="64px"
-						height="64px"
-						viewBox="-0.5 0 48 48"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-						xmlns:xlink="http://www.w3.org/1999/xlink"
-						fill="#000000"
-						class="w-5 h-5"
-						><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
-							id="SVGRepo_tracerCarrier"
-							stroke-linecap="round"
-							stroke-linejoin="round" /><g id="SVGRepo_iconCarrier">
-							<title>Google-color</title> <desc>Created with Sketch.</desc> <defs />
-							<g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-								<g id="Color-" transform="translate(-401.000000, -860.000000)">
-									<g id="Google" transform="translate(401.000000, 860.000000)">
-										<path
-											d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
-											id="Fill-1"
-											fill="#FBBC05" />
-										<path
-											d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
-											id="Fill-2"
-											fill="#EB4335" />
-										<path
-											d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
-											id="Fill-3"
-											fill="#34A853" />
-										<path
-											d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
-											id="Fill-4"
-											fill="#4285F4" />
-									</g>
-								</g>
-							</g>
-						</g></svg>
-					<span class="flex-1 ml-3 whitespace-nowrap">Sign in with Google</span>
-				</div>
-			</li>
-			<li>
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<div
-					on:click={() => signIn('facebook')}
-					class="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg cursor-pointer dark:text-white dark:bg-gray-600 hover:bg-gray-100 hover:shadow group dark:hover:bg-gray-500">
-					<svg
-						width="64px"
-						height="64px"
-						viewBox="0 0 16 16"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						class="w-5 h-5"
-						><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
-							id="SVGRepo_tracerCarrier"
-							stroke-linecap="round"
-							stroke-linejoin="round" /><g id="SVGRepo_iconCarrier"
-							><path
-								fill="#1877F2"
-								d="M15 8a7 7 0 00-7-7 7 7 0 00-1.094 13.915v-4.892H5.13V8h1.777V6.458c0-1.754 1.045-2.724 2.644-2.724.766 0 1.567.137 1.567.137v1.723h-.883c-.87 0-1.14.54-1.14 1.093V8h1.941l-.31 2.023H9.094v4.892A7.001 7.001 0 0015 8z" /><path
-								fill="#ffffff"
-								d="M10.725 10.023L11.035 8H9.094V6.687c0-.553.27-1.093 1.14-1.093h.883V3.87s-.801-.137-1.567-.137c-1.6 0-2.644.97-2.644 2.724V8H5.13v2.023h1.777v4.892a7.037 7.037 0 002.188 0v-4.892h1.63z" /></g
-						></svg>
-					<span class="flex-1 ml-3 whitespace-nowrap">Sign in with Facebook</span>
-				</div>
-			</li>
-		</ul>
+		<div class="my-4 space-y-3">
+			<GitHubSignInButton />
+			<GoogleSignInButton />
+			<FacebookSignInButton />
+		</div>
 	</form>
 </Card>
