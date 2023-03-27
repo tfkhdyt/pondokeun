a99acc8e5c624527f20d5e8eddf45abef12a42d7
tfkhdyt <tfkhdyt@proton.me>
2023-03-28 05:27:36 +0700

    feat(root-page): show more toast on success and error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,9 @@
 	}
 
 	$: if (form?.success) {
+		toast.success(`/${form.addedLink.slug} has been created successfully`, {
+			position: 'top-right',
+		});
 		formS.set({ link: '', customName: '' });
 	}
 </script>
@@ -29,66 +32,70 @@
 	<title>Pondokeun</title>
 </svelte:head>
 
-<CTA />
-<form method="POST" use:enhance class="mt-8" use:autoAnimate>
-	<label for="link" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-		>Paste a link to shorten it</label>
+<main>
+	<CTA />
+	<form method="POST" use:enhance class="mt-8" use:autoAnimate>
+		<label for="link" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+			>Paste a link to shorten it</label>
 
-	<Input
-		id="link"
-		placeholder="Paste a link to shorten it"
-		size="lg"
-		name="link"
-		bind:value={$formS.link}
-		{...$constraints.link}
-		color={$errors.link || (form?.success && form?.message) ? 'red' : 'base'}>
-		<svg
-			slot="left"
-			aria-hidden="true"
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6 text-gray-500 dark:text-gray-400">
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-		</svg>
-		<Button slot="right" size="md" type="submit">Shorten</Button>
-	</Input>
+		<Input
+			id="link"
+			placeholder="Paste a link to shorten it"
+			size="lg"
+			name="link"
+			bind:value={$formS.link}
+			{...$constraints.link}
+			color={$errors.link || (form?.success && form?.message) ? 'red' : 'base'}>
+			<svg
+				slot="left"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6 text-gray-500 dark:text-gray-400">
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+			</svg>
+			<Button slot="right" size="md" type="submit">Shorten</Button>
+		</Input>
 
-	{#if $errors.link}
-		<Helper class="mt-2" color="red">{$errors.link.join(', ')}</Helper>
-	{/if}
-	{#if $page.data.session?.user}
-		<Toggle class="my-4" bind:checked={isUseCustomSlug}>Use custom slug</Toggle>
-		{#if isUseCustomSlug}
-			<Input
-				type="text"
-				placeholder="Your custom slug (Alphanumeric, hyphen, and underscore only)"
-				size="lg"
-				name="customName"
-				bind:value={$formS.customName}
-				{...$constraints.customName}
-				pattern="[a-zA-Z0-9_-]*" />
-			{#if $errors.customName}
-				<Helper class="mt-2" color="red">{$errors.customName.join(', ')}</Helper>
+		{#if $errors.link}
+			<Helper class="mt-2" color="red">{$errors.link.join(', ')}</Helper>
+		{/if}
+		{#if $page.data.session?.user}
+			<Toggle class="my-4" bind:checked={isUseCustomSlug}>Use custom slug</Toggle>
+			{#if isUseCustomSlug}
+				<Input
+					type="text"
+					placeholder="Your custom slug (Alphanumeric, hyphen, and underscore only)"
+					size="lg"
+					name="customName"
+					bind:value={$formS.customName}
+					{...$constraints.customName}
+					color={form?.message?.includes('has been used') ? 'red' : 'base'}
+					pattern="[a-zA-Z0-9_-]*" />
+
+				{#if $errors.customName}
+					<Helper class="mt-2" color="red">{$errors.customName.join(', ')}</Helper>
+				{/if}
 			{/if}
 		{/if}
-	{/if}
-</form>
+	</form>
 
-<div class="mt-4 space-y-2" use:autoAnimate>
-	{#if form?.success === true && !$page.data.session}
-		<SingleResult
-			slug={form.addedLink.slug}
-			link={form.addedLink.link}
-			createdDate={form.addedLink.createdAt} />
-	{:else if $page.data.session && data.links && data.links.length > 0}
-		{#each data.links as link (link.id)}
-			<SingleResult slug={link.slug} link={link.link} createdDate={link.createdAt} />
-		{/each}
-	{/if}
-</div>
+	<div class="mt-4 space-y-2" use:autoAnimate>
+		{#if form?.success === true && !$page.data.session}
+			<SingleResult
+				slug={form.addedLink.slug}
+				link={form.addedLink.link}
+				createdDate={form.addedLink.createdAt} />
+		{:else if $page.data.session && data.links && data.links.length > 0}
+			{#each data.links as link (link.id)}
+				<SingleResult slug={link.slug} link={link.link} createdDate={link.createdAt} />
+			{/each}
+		{/if}
+	</div>
+</main>
