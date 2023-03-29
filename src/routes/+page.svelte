5b01c8428d7613acdee26398c19aa8f05c61912f
tfkhdyt <tfkhdyt@proton.me>
2023-03-30 05:31:04 +0700

    fix(root-page): change link input field

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import autoAnimate from '@formkit/auto-animate';
 	import type { Link } from '@prisma/client';
-	import { Button, FloatingLabelInput, Helper, Input, Select, Toggle } from 'flowbite-svelte';
+	import {
+		Button,
+		ButtonGroup,
+		FloatingLabelInput,
+		Helper,
+		Input,
+		Select,
+		Toggle,
+	} from 'flowbite-svelte';
 	import toast from 'svelte-french-toast';
 	import { superForm } from 'sveltekit-superforms/client';
 
@@ -71,37 +79,23 @@
 <main>
 	<CTA />
 	<form method="POST" use:enhance class="mt-8" use:autoAnimate>
-		<label for="link" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-			>Paste a link to shorten it</label>
+		<div>
+			<ButtonGroup class="w-full">
+				<Input
+					id="link"
+					type="url"
+					name="link"
+					bind:value={$formS.link}
+					{...$constraints.link}
+					color={$errors.link || (form?.success && form?.message) ? 'red' : 'base'}
+					placeholder="Paste a link to shorten it" />
+				<Button color="blue" type="submit">Shorten</Button>
+			</ButtonGroup>
+			{#if $errors.link}
+				<Helper class="mt-2" color="red">{$errors.link.join(', ')}</Helper>
+			{/if}
+		</div>
 
-		<Input
-			id="link"
-			placeholder="Paste a link to shorten it"
-			size="md"
-			name="link"
-			bind:value={$formS.link}
-			{...$constraints.link}
-			color={$errors.link || (form?.success && form?.message) ? 'red' : 'base'}>
-			<svg
-				slot="left"
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-6 h-6 text-gray-500 dark:text-gray-400">
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-			</svg>
-			<Button slot="right" size="xs" type="submit">Shorten</Button>
-		</Input>
-
-		{#if $errors.link}
-			<Helper class="mt-2" color="red">{$errors.link.join(', ')}</Helper>
-		{/if}
 		{#if $page.data.session?.user}
 			<Toggle class="my-4" bind:checked={isUseCustomSlug}>Use custom slug</Toggle>
 			{#if isUseCustomSlug}
