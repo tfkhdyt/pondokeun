a4868d120bbfecce533708b915937b40f4547f56
tfkhdyt <tfkhdyt@proton.me>
2023-03-29 17:55:00 +0700

    feat(auth): add google provider

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,23 @@
 import type { Provider } from '@auth/core/providers';
 import GitHub from '@auth/core/providers/github';
+import Google from '@auth/core/providers/google';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import type { Handle } from '@sveltejs/kit';
 
 import { db } from '$db/prisma';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
 
 export const handle = SvelteKitAuth({
 	adapter: PrismaAdapter(db),
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })] as Provider[],
+	providers: [
+		GitHub({
+			clientId: GITHUB_ID,
+			clientSecret: GITHUB_SECRET,
+		}),
+		Google({
+			clientId: GOOGLE_ID,
+			clientSecret: GOOGLE_SECRET,
+		}),
+	] as Provider[],
 }) satisfies Handle;
