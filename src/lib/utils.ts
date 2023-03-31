4e951d89f9b834ff9318d185b1059241e12c5189
tfkhdyt <tfkhdyt@proton.me>
2023-03-31 19:43:38 +0700

    refactor(utils.ts): use trpc client to delete link and handle errors with TRPCClientError refactor(+server.ts): remove unused imports and delete DELETE request handler for /api/links/[slug] endpoint

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,18 @@
+import { TRPCClientError } from '@trpc/client';
 import toast from 'svelte-french-toast';
 
 import { invalidate } from '$app/navigation';
 
+import { trpc } from './trpc/client';
+
 export const deleteLink = async (slug: string) => {
-	const res = await fetch(`/api/links/${slug}`, {
-		method: 'DELETE',
-	});
-	const data = await res.json();
-
-	if (!res.ok) {
-		toast.error(data.message, { position: 'top-right' });
-		return;
+	try {
+		await trpc().deleteLink.mutate({ slug });
+		await invalidate('links');
+		toast.success(`/${slug} has been deleted successfully`, { position: 'top-right' });
+	} catch (error) {
+		if (error instanceof TRPCClientError) {
+			toast.error(error.message, { position: 'top-right' });
+		}
 	}
-
-	toast.success(data.message, { position: 'top-right' });
-	invalidate('links');
 };
