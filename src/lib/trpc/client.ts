7c999b3e322e217bacc7b9fe3aab358378d7387f
tfkhdyt <tfkhdyt@proton.me>
2023-03-31 17:38:47 +0700

    feat(client.ts): add SuperJSON as a transformer for trpc client requests

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -1,3 +1,4 @@
+import SuperJSON from 'superjson';
 import { createTRPCClient, type TRPCClientInit } from 'trpc-sveltekit';
 
 import type { Router } from '$lib/trpc/router';
@@ -7,7 +8,7 @@ let browserclient: ReturnType<typeof createTRPCClient<Router>>;
 export function trpc(init?: TRPCClientInit) {
 	const isBrowser = typeof window !== 'undefined';
 	if (isBrowser && browserclient) return browserclient;
-	const client = createTRPCClient<Router>({ init });
+	const client = createTRPCClient<Router>({ init, transformer: SuperJSON });
 	if (isBrowser) browserclient = client;
 	return client;
 }
