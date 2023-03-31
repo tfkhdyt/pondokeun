bdf47744d06723983b6fd243994d9a45a621fb99
tfkhdyt <tfkhdyt@proton.me>
2023-03-31 18:44:21 +0700

    refactor(page): move page load function from +page.ts to +page.server.ts feat(page): add server-side rendering support for page load function.

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+import { createContext } from '$lib/trpc/context';
+import { router } from '$lib/trpc/router';
+
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async (event) => {
+	event.depends('links');
+
+	return {
+		links: router.createCaller(await createContext(event)).getAllLinks(),
+	};
+};
