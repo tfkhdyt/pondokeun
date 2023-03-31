b0c707efab1c1e7367da687ee65a2ede050798b0
tfkhdyt <tfkhdyt@proton.me>
2023-03-31 17:39:32 +0700

    feat(router.ts): add createLink mutation to router with input validation and error handling

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
 
 import { initTRPC, TRPCError } from '@trpc/server';
+import SuperJSON from 'superjson';
 
 import { container } from '$containers/inversify.container';
+import { linkSchema } from '$entities/link.entity';
 import type { ILinkService } from '$services/link.service';
 import { TYPES } from '$types/inversify.type';
 
@@ -10,7 +12,9 @@ import type { Context } from './context';
 
 const linkService = container.get<ILinkService>(TYPES.ILinkService);
 
-export const t = initTRPC.context<Context>().create();
+export const t = initTRPC.context<Context>().create({
+	transformer: SuperJSON,
+});
 
 export const router = t.router({
 	getAllLinks: t.procedure.query(async ({ ctx }) => {
@@ -24,6 +28,43 @@ export const router = t.router({
 		}
 
 		return links;
+	}),
+	createLink: t.procedure.input(linkSchema).mutation(async ({ input, ctx }) => {
+		const { link, customName } = input;
+		const { session } = ctx;
+
+		if (customName) {
+			if (!session?.user) {
+				throw new TRPCError({
+					code: 'UNAUTHORIZED',
+					message: 'You should sign in first',
+				});
+			}
+
+			const err = await linkService.verifySlugAvailability(customName);
+			if (err instanceof Error) {
+				throw new TRPCError({
+					code: 'BAD_REQUEST',
+					message: err.message,
+					cause: err,
+				});
+			}
+		}
+
+		const [addedLink, err] = await linkService.createLink({
+			link,
+			slug: customName,
+			email: session?.user?.email,
+		});
+		if (err instanceof Error) {
+			throw new TRPCError({
+				code: 'INTERNAL_SERVER_ERROR',
+				message: err.message,
+				cause: err,
+			});
+		}
+
+		return addedLink;
 	}),
 });
 
