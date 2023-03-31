757d3ee7e8ea6ce01bf308b30ce9ffc51395f53b
tfkhdyt <tfkhdyt@proton.me>
2023-03-31 17:39:57 +0700

    refactor(+page.server.ts): remove unused imports and actions from page server route

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,10 @@
-import { type Actions, fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
 
-import { container } from '$containers/inversify.container';
 import { linkSchema } from '$entities/link.entity';
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
-import type { ILinkService } from '$services/link.service';
-import { TYPES } from '$types/inversify.type';
 
 import type { PageServerLoad } from './$types';
-
-const linkService = container.get<ILinkService>(TYPES.ILinkService);
 
 export const load = (async (event) => {
 	event.depends('links');
@@ -24,47 +18,3 @@ export const load = (async (event) => {
 
 	return { form };
 }) satisfies PageServerLoad;
-
-export const actions = {
-	default: async (event) => {
-		const form = await superValidate(event, linkSchema);
-
-		if (!form.valid) {
-			return fail(400, { form, message: 'Invalid input' });
-		}
-
-		const { link, customName } = form.data;
-		const session = await event.locals.getSession();
-
-		if (customName) {
-			if (!session?.user) {
-				return fail(401, {
-					message: 'You should sign in first',
-					form,
-				});
-			}
-
-			const err = await linkService.verifySlugAvailability(customName);
-			if (err instanceof Error) {
-				return fail(400, {
-					message: err.message,
-					form,
-				});
-			}
-		}
-
-		const [addedLink, err] = await linkService.createLink({
-			link,
-			slug: customName,
-			email: session?.user?.email,
-		});
-		if (err instanceof Error) {
-			return fail(400, {
-				message: err.message,
-				form,
-			});
-		}
-
-		return { form, addedLink, success: true };
-	},
-} satisfies Actions;
