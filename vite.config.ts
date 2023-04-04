5fb13e555bb98a69493338ecf89517ed77f60538
tfkhdyt <tfkhdyt@proton.me>
2023-04-04 08:44:02 +0700

    feat: setup vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+			includeAssets: ['./static/favicon.png', './static/pondokeun-192.png'],
+			manifest: {
+				name: 'Pondokeun - Link Shortener',
+				short_name: 'Pondokeun',
+				description: 'Modern, Lightweight, Free and Open Source Link Shortener',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: './static/pondokeun-192.png',
+						size: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: './static/pondokeun-512.png',
+						size: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
